8af40e12711ae994a11c23bc6466d53f980923ba
Subhasis <subhasispatra94@gmail.com>
2021-10-17 17:28:43 +0530

    added loop for day 3

---
+++ day3 /loop.js
@@ -0,0 +1,85 @@
+
+
+// even odd numer 
+
+let number = 18 
+// range 0 - 10 
+// 10 - 20 
+// 20 30 
+// 30 - 40 
+
+//if(number < 10  ){
+//    console.log("Less than 10")
+//}
+//if(number < 20 && number > 10  ){
+//    console.log("less than 20")
+//}
+//if(number < 30 && number > 20 ){
+//    console.log("less than 30")
+//}
+
+// if , else if , else
+
+
+//if (80 < mark && mark < 90 ) {
+//    console.log("Grade A")
+//} 
+//if (90 < mark && mark < 100 ) {
+//    console.log("Grade O")
+//} 
+
+/*
+
+mark = 70
+if(mark < 40){ // mark = 40 < 40 - false 
+console.log("Fail")
+}else if (mark < 70) {
+console.log("c or pass ")
+}else if (mark < 80 ) {
+console.log("B")
+}else if (mark < 90) {
+console.log("A")
+}else{
+console.log("O")
+}
+*/
+
+// Loops in JS 
+
+// two types of loop 
+// for and while 
+
+// for loop 
+//  ( initialization ; condition ; incriment / decrement )
+
+// all the odd number between 1 and 10 
+
+/*
+
+for(i = 1 ; i < 11 ; i++)
+{
+if(i % 2 !== 0 )
+{
+console.log(i)
+
+}
+} 
+*/
+
+// While loop 
+
+/*
+i = 0 // initialize 
+while (i < 10) { // condition 
+if( i %2 === 0){
+console.log(i)
+}
+
+i++  // last line // incriment 
+}
+*/
+
+
+
+
+
